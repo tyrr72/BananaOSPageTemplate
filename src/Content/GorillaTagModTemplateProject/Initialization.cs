c928787dd898a773480a294a6d894135b18e03ba
Tyre <122072344+tyrr72@users.noreply.github.com>
2024-08-11 19:03:07 +1000

    Update and rename Plugin.cs to Initialization.cs

---
+++ src/Content/GorillaTagModTemplateProject/Initialization.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using BepInEx;
+using UnityEngine;
+using Utilla;
+
+namespace GorillaTagModTemplateProject{
+	
+	[ModdedGamemode]
+	[BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+	public class Initialization : BaseUnityPlugin
+	{
+ 		// To access this go to the Page.cs and do Initialization.inRoom 
+		public static bool inRoom;
+
+		void Start()
+		{
+			Utilla.Events.GameInitialized += OnGameInitialized;
+		}
+
+		void OnEnable()
+		{
+			HarmonyPatches.ApplyHarmonyPatches();
+		}
+
+		void OnDisable()
+		{
+			HarmonyPatches.RemoveHarmonyPatches();
+		}
+
+		void OnGameInitialized(object sender, EventArgs e)
+		{
+		}
+
+		void Update()
+		{
+		}
+
+		[ModdedGamemodeJoin]
+		public void OnJoin(string gamemode)
+		{
+			inRoom = true;
+		}
+
+		[ModdedGamemodeLeave]
+		public void OnLeave(string gamemode)
+		{
+			inRoom = false;
+		}
+	}
+}
